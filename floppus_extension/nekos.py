416538dec28da3e29f80f6d291c8e8e670640427
WeMayNeverKnow <77531972+WeMayNeverKnow@users.noreply.github.com>
2021-04-26 11:45:22 +0800

    Removed cat.py stuff because it is kinda useless now. Made a new stuff but still bad code but future me will fix it I hope please do though

--- floppus_extension/nekos.py
+++ floppus_extension/nekos.py
@@ -1,73 +1,193 @@
 import os
 import random
 
-from hata import Client, Embed, Guild
+from hata import Client, Embed, Guild, chunkify
 from hata.ext.commands import Pagination
 import hata.ext.asyncio
 
-from cat import Client as CatClient
 from config import CAT_API_KEY
 from bot_utils.utils import get_event_color
 from bot_utils.shared_data import TEST_GUILD
 
 
 Floppus: Client
-MyNeko = CatClient(CAT_API_KEY)
+
+CAT_API_URL = 'https://api.thecatapi.com/v1'
 
 
-ORDER_CHOICES = {
-  'Descending' : 'desc',
-  'Ascending'  : 'asc' ,
-  'Random'     : 'rand',
- }
+async def get_cat(route, params):
+  """requests the api for the given params"""
+  headers = {'x-api-key': CAT_API_KEY}
+  
+  async with Floppus.http.get(CAT_API_URL+route, params = params, headers = headers) as response:
+    cat_data = await response.json()
+    return cat_data
 
-@Floppus.interactions(guild=TEST_GUILD)
-async def neko(client, event,
+
+#Slash Area
+
+IMAGE_TYPES = {'Png': 'png', 'Jpg': 'jpg', 'Gif': 'gif',}
+ORDER_CHOICES = {'Descending': 'desc', 'Ascending': 'asc', 'Random': 'rand',}
+
+#A main slash command
+FLOPPUS = Floppus.interactions(None, name='neko', description='neko stuff', guild = TEST_GUILD)
+
+
+#Sub slash commands
+
+@FLOPPUS.interactions
+async def gimme(client, event,
+    image_type : (IMAGE_TYPES, 'What type of image?') = None,
     limit: ('int', 'How many cats?') = 10,
     page : ('int', 'Jump to page..?') = 0,
-    order: (ORDER_CHOICES, 'In what order?') = 'rand',
+    order: (ORDER_CHOICES, 'In what order?') = 'asc',
   ):
   """Gives you nekos"""
   yield
   
-  cats = await MyNeko.get_cat(limit, page, order)
+  params = {'mime_types': image_type, 'limit': limit, 'page': page, 'order': order}
+  cats = await get_cat('/images/search', params)
   
   color = get_event_color(event)
   pages = []
   
   for x, cat in enumerate(cats):
+    cat_url = cat['url']
     
-    if breeds := cat.breeds:
+    #checks for breeds
+    if breeds := cat.get('breeds'):
       desc = \
-        f', Breed Name: {",".join(breed.name for breed in breeds)}'\
-        f', Breed Id: {",".join(breed.id for breed in breeds)}'
+        f'\nBreed Name: {",".join(breed["name"] for breed in breeds)}'\
+        f'\nBreed Id: {",".join(breed["id"] for breed in breeds)}'
     else:
       desc = ''
     
-    pages.append(Embed('Here is your neko nya!', url = cat.url, color = color)\
-      .add_image(cat.url)\
-      .add_footer(f'Page: {x+1}/{len(cats)}\nCat Id: {cat.id} {desc}')
+    #checks for categories
+    if categories := cat.get('categories'):
+      category = \
+        f'\nCategory Name: {",".join(str(category["name"]) for category in categories)}'\
+        f'\nCategory Id: {",".join(str(category["id"]) for category in categories)}'
+    else:
+      category = ''
+    
+    pages.append(Embed('Here is your neko nya!', url = cat_url, color = color)\
+      .add_image(cat_url)\
+      .add_footer(f'Page: {x+1}/{len(cats)}\nCat Id: {cat["id"]} {desc} {category}')
     )
+  
+  #If api didn't return any cats
+  if not pages:
+    pages.append(Embed('GomenNyaSai !!!', 'There seems to be no neko today.' , color = color))
   
   await Pagination(client, event, pages)
 
 
-@Floppus.commands
-async def cat_gui(client, message):
-  """
-  The cat gui for the cat stuff
-  """
+CHOICES = {
+  'Category':'category',
+  'Breed':'breed',
+}
+
+@FLOPPUS.interactions
+async def search(client, event,
+    choice: (CHOICES, 'What do you wanna search for?'),
+    id_  : ('str', 'The id of choice'),
+    limit: ('int', 'How many cats?') = 10,
+    page : ('int', 'Jump to page..?') = 0,
+    order: (ORDER_CHOICES, 'In what order?') = 'asc',
+  ):
+  """by category or by breed"""
+  yield
+  
+  params = {'limit': limit, 'page': page, 'order': order}
+  params[('category_ids') if choice == 'category' else ('breeds_ids')] = id_
+  
+  cats = await get_cat('/images/search', params)
+  
+  color = get_event_color(event)
+  pages = []
+  
+  for x, cat in enumerate(cats):
+    cat_url = cat['url']
+    
+    #checks for breeds
+    if breeds := cat.get('breeds'):
+      desc = \
+        f'\nBreed Name: {",".join(breed["name"] for breed in breeds)}'\
+        f'\nBreed Id: {",".join(breed["id"] for breed in breeds)}'
+    else:
+      desc = ''
+    
+    #checks for categories
+    if categories := cat.get('categories'):
+      category = \
+        f'\nCategory Name: {",".join(str(category["name"]) for category in categories)}'\
+        f'\nCategory Id: {",".join(str(category["id"]) for category in categories)}'
+    else:
+      category = ''
+    
+    pages.append(Embed('Here is your neko nya!', url = cat_url, color = color)\
+      .add_image(cat_url)\
+      .add_footer(f'Page: {x+1}/{len(cats)}\nCat Id: {cat["id"]} {desc} {category}')
+    )
+  
+  #If api didn't return any cats
+  if not pages:
+    pages.append(Embed('GomenNyaSai !!!', 'There seems to be no neko for that.' , color = color))
+  
+  await Pagination(client, event, pages)
 
 
-@Floppus.commands
-async def find_breed(client, message):
-  """
-  Finds the given breed of the cat
-  """
+@FLOPPUS.interactions
+async def list_(client, event,
+    choice: (CHOICES, 'What do you want to list?'),
+    limit: ('int', 'How many cats?') = 10,
+    page : ('int', 'Jump to page..?') = 0,
+    order: (ORDER_CHOICES, 'In what order?') = 'asc',
+  ):
+  """list the category or breed"""
+  yield
+  
+  params = {'limit': limit, 'page': page, 'order': order}
+  
+  if choice == 'category':
+    choice = 'categories'
+  else:
+    choice = 'breeds'
+  
+  stuff = await get_cat(f'/{choice}', params)
+  
+  string = []
+  
+  for num, dictionary in enumerate(stuff):
+    string.append(f'{num + 1}. __**Name:**__ {dictionary["name"]}\n    __**Id:**__ {dictionary["id"]}\n')
+  
+  color = get_event_color(event)
+  pages = [(Embed(f'Results for {choice}', details, color).add_footer(f'Page: {num+1}')) for num, details in enumerate(chunkify(string))]
+  await Pagination(client, event, pages)
 
 
-@Floppus.commands
-async def find_cat(client, message):
-  """
-  Finds that cat using the given id of the cat
-  """
+@FLOPPUS.interactions
+async def breed_info(client, event,
+  breed_id: ('str', 'What is the cat id?'),
+):
+  """get the info of the breed"""
+  yield
+  
+  color = get_event_color(event)
+  string = []
+  pages = []
+  
+  params = {'q':breed_id}
+  breeds = await get_cat('/breeds', params)
+  
+  if not breeds:
+    pages.append(Embed('GomenNyaSai !!!', 'There seems to be no neko for that.' , color = color))
+    await Pagination(client, event, pages)
+  
+  for breed in breeds:
+    for key, value in breed.items():
+      string.append(f'__**{key}:**__\n{value}\n')
+  
+    pages = [(Embed(f'Results for {breed_id}', details, color).add_footer(f'Page: {num+1}')) for num, details in enumerate(chunkify(string))]
+  
+  await Pagination(client, event, pages)
